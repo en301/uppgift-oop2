aa5bdfd8b21aa6c414d22754705c1be7494ce6b8
en301 <emilsjostrand1000@gmail.com>
2024-04-03 11:00:24 +0200

    Add project files.

---
+++ uppgift-oop2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace uppgift_oop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            namn p = new namn();
+            Console.WriteLine("Förnamn?");
+            p.Förnamm = Console.ReadLine();
+            Console.WriteLine("Efternamn?");
+            p.Efternamn = Console.ReadLine();
+            Console.WriteLine("");
+            p.Namn();
+        }
+    }
+}
+
